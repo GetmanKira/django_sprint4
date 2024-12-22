6da8331ea07f86a748ea040264f442c2f1a55412
GetmanKira <kirochkagetman@gmail.com>
2024-12-22 23:20:56 +0300

    Update forms.py
    комментарии

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,3 +1,5 @@
+"""определяет формы связанные с моделями приложения
+формы обрабатывают пользовательский ввод и обработывают данные"""
 from django import forms
 from django.utils import timezone
 
@@ -8,20 +10,21 @@ class PostForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(PostForm, self).__init__(*args, **kwargs)
-        self.fields['pub_date'].initial = timezone.now()
+        self.fields['pub_date'].initial = timezone.now()#установка начального значения на текущее время
 
-    class Meta:
+
+    class Meta:#определяет мета информацию формы
         model = Post
         exclude = ('author',)
         widgets = {
             'pub_date': forms.DateTimeInput(attrs={
                 'type': 'datetime-local',
                 'format': '%Y-%m-%dT%H:%M'
-            }),
+            }),#натсройка виджетов для полей формы
         }
 
 
-class CommentForm(forms.ModelForm):
+class CommentForm(forms.ModelForm):#форма для экземпляров модели comment
     class Meta:
         model = Comment
         exclude = ('author', 'post', 'is_published')
