b5b1c7ea3e4932c666eaba96a58cfdbd586119fe
GetmanKira <kirochkagetman@gmail.com>
2024-12-22 23:17:51 +0300

    Update admin.py
    добавила комментарии

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,14 +1,20 @@
+""" Файл admin.py предназначен для настройки интерфейса а
+дминистрирования для моделей приложения Django.
+В этом файле определяется, как различные модели будут
+представлены и управляться в админке"""
 from django.contrib import admin
 
 from .models import Category, Comment, Location, Post
 
-LENGTH_STRING = 50
-NUMBER_OF_POSTS = 10
+LENGTH_STRING = 50  # Максимальная длина строки для отображения
+NUMBER_OF_POSTS = 10  # Количество постов на странице
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    """Кастомизация админки для модели Post."""
+    """Кастомизация админки для модели Post.
+    PostAdmin — класс, отвечающий за интерфейс
+    управления моделью Post в админке Django"""
 
     list_display = (
         'title',
@@ -39,7 +45,9 @@ class PostAdmin(admin.ModelAdmin):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-
+    """CategoryAdmin — класс для настройки
+     отображения и управления моделью Category
+    в админке."""
     list_display = (
         'title',
         'description_short',
@@ -64,7 +72,8 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    """Кастомизация админки для модели Location."""
+    """Кастомизация админки для модели Location.
+    LocationAdmin — класс для администрирования модели Location."""
 
     list_display = (
         'name',
@@ -78,7 +87,9 @@ class LocationAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    """Кастомизация админки для модели Comment."""
+    """CommentAdmin — класс, который управляет отображением
+    и функционалом модели Comment в админке. Он отвечает за
+    интерфейс работы с комментариями."""
 
     list_display = (
         'text',
