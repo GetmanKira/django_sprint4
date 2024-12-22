dbfe0bd5d70438c49cc394d3f93da1874bb23337
GetmanKira <kirochkagetman@gmail.com>
2024-12-22 23:19:05 +0300

    Update apps.py
    добавлен комментарий

--- blogicum/blog/apps.py
+++ blogicum/blog/apps.py
@@ -1,5 +1,7 @@
+"""содержит конфигурацию приложения
+используется для определения параметров, связанных с приложением
+ """
 from django.apps import AppConfig
-
 
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
